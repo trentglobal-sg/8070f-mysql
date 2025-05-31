780db20c03a2f023e0806471a3fe0c2878d2f3d1
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-05-31 08:36:22 +0000

    DDL

--- 01-ddl/ddl.sql
+++ 01-ddl/ddl.sql
@@ -22,3 +22,40 @@ SHOW TABLES;
 
 -- show the columns of a table
 DESCRIBE parents;
+
+-- create the student table
+create table students (
+ student_id int unsigned primary key auto_increment,
+ name varchar(100) not null,
+ gender varchar(2) not null,
+ swimming_grade tinyint unsigned,
+ date_of_birth datetime
+) engine = innodb;
+
+-- ALTER TABLE:MODIFY COLUMN: make changes to the columns of an existing table
+alter table students modify column date_of_birth datetime not null;
+
+-- ALTER TABLE:ADD COLUMN: add a new column to a table
+alter table students add column parent_id int unsigned not null;
+
+-- ALTER TABLE: ADD CONSTRAINT
+-- configure students.parent_id column as FK to parents.parent_id column
+alter table students add constraint fk_parents_students
+      foreign key (parent_id) references parents(parent_id);
+
+-- INSERTing rows into a table (Data Manipulation Language)
+insert into parents (name, contact_number, address) values ("Tan Ah Kow", "1231234", "Yishun Ring Road");
+
+-- We use the DQL (Data Query Language) to see the content of a table
+select * FROM parents;
+
+-- INSERT A student; every student needs a FK to the parent
+insert into students (name, gender, swimming_grade, date_of_birth, parent_id)
+ values ("Tan Ah Mew", "f", 1, "2024-05-31", 1);
+
+ -- add a temporary students that is to be deleted
+ alter table students add column test varchar(100);
+
+-- ALTER TABLE: DROP COLUMN
+ -- to delete a column
+ alter table students drop column test;
